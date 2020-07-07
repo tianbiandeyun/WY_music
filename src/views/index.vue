0528cc6115dd568d7b676a6c23da92c28ebd2c5f
zed <158178152@qq.com>
2020-07-07 18:33:25 +0800

    配置必须的插件

---
+++ src/views/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <section class="index">
+        大家好
+    </section>
+</template>
+
+<script>
+
+    export default {
+        name: 'index',
+        components: {}
+    }
+</script>
+<style lang="less" scoped>
+    .index {
+        color: @default_color;
+    }
+</style>
