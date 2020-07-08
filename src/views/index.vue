b67c1ce1fcba87d679d65eab74dda88647a0d32d
zed <158178152@qq.com>
2020-07-08 17:27:18 +0800

    配置 vant 但是遇到问题

--- src/views/index.vue
+++ src/views/index.vue
@@ -3,21 +3,32 @@
 
         <!--<wy_swiper></wy_swiper>-->
 
+        <CellGroup>
+            <Cell title="单元格" value="内容"/>
+            <Cell title="单元格" value="内容" label="描述信息"/>
+        </CellGroup>
+
     </section>
 </template>
 
 <script>
+    import {Cell, CellGroup} from 'vant';
     import wy_swiper from '../components/WY_swiper/wy_swiper.vue'
 
     export default {
         name: 'index',
-        components: {wy_swiper},
+        components: {wy_swiper, Cell, CellGroup},
         created() {
-            this.$store.dispatch("_getBanner", {
-                im: this.$config.request_face.banner
-            }).then(res => {
-                console.log(res)
-            })
+
+        },
+        methods: {
+            request_index() {
+                this.$store.dispatch("_getBanner", {
+                    im: this.$config.request_face.banner
+                }).then(res => {
+                    console.log(res)
+                })
+            }
         }
     }
 </script>
