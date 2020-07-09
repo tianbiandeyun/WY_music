9e03f0cc6544eb2025d9c799fd536732c7455d12
zed <158178152@qq.com>
2020-07-09 19:22:16 +0800

    配置 jest 但是遇到 import 引入不成功的问题

---
+++ tests/unit/cel_test.spec.js
@@ -0,0 +1,9 @@
+import {mount} from '@vue/test-utils'
+// import loading from '../../src/components/WY_loading/wy_loading'
+
+describe('loading.vue', () => {
+    it('测试一下，更健康', () => {
+        const msg = 'new message';
+        console.log(msg)
+    })
+});
