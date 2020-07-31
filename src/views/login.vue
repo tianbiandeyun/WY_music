cc322ad6f1ed0f03b61fd843a8814d0cfd066734
zed <158178152@qq.com>
2020-07-31 14:38:44 +0800

    .

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,15 +1,34 @@
 <template>
     <section class="login-container">
 
-        <div class="login-message">
+        <!--<div class="login-message">-->
+
+        <!--</div>-->
+
+        <!--<div class="button-container">-->
+        <!--<Button round block type="primary" size="small" @click="submit">提交</Button>-->
+        <!--</div>-->
+
+        <!--<p>{{new_size}}</p>-->
+
+
+        <div class="grid-box">
+
+            <div class="header">
+                header
+            </div>
+
+            <div class="content">
+                <div class="left-slide">left-slide</div>
+                <div class="main-content">main-content</div>
+                <div class="right-slide">right-slide</div>
+            </div>
+
+            <div class="footer">
+                footer
+            </div>
 
         </div>
-
-        <div class="button-container">
-            <Button round block type="primary" size="small" @click="submit">提交</Button>
-        </div>
-
-        <p>{{new_size}}</p>
 
     </section>
 </template>
@@ -65,26 +84,63 @@
 
 <style lang="less" scoped>
 
-    .login-container {
-        position: relative;
-        height: 100%;
+    .grid-box {
+        border: 1px solid black;
+        width: 960px;
+        height: 400px;
+        margin: 0 auto;
+        color: #000;
+        text-align: center;
+        font-size: 20px;
+        /*---*/
+        display: grid;
+        grid-template-rows: 1fr 3fr 1fr;
 
-        .login-message {
-            .default-box-sizing;
-            width: 100%;
-            position: absolute;
-            left: 0;
-            top: 200px;
-            border: 1px solid black;
+        .header {
+            background-color: #ff9900;
         }
 
-        .button-container {
-            .default-padding-10-20();
-            .default-box-sizing;
-            position: fixed;
-            bottom: 0;
-            left: 0;
-            width: 100%;
+        .content {
+            display: grid;
+            grid-template-columns: 100px auto 100px;
+            grid-column-gap: 20px;
+
+            .left-slide {
+                background-color: #808695;
+            }
+            .main-content {
+                background-color: #515a6e;
+            }
+            .right-slide {
+                background-color: #c5c8ce;
+            }
+        }
+
+        .footer {
+            background-color: bisque;
         }
     }
+
+    /*.login-container {*/
+    /*position: relative;*/
+    /*height: 100%;*/
+
+    /*.login-message {*/
+    /*//.default-box-sizing;*/
+    /*width: 100%;*/
+    /*position: absolute;*/
+    /*left: 0;*/
+    /*top: 200px;*/
+    /*border: 1px solid black;*/
+    /*}*/
+
+    /*.button-container {*/
+    /*//.default-padding-10-20();*/
+    /*//.default-box-sizing;*/
+    /*position: fixed;*/
+    /*bottom: 0;*/
+    /*left: 0;*/
+    /*width: 100%;*/
+    /*}*/
+    /*}*/
 </style>
