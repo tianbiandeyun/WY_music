1fd25d6c460c49e2bd4de6eaa68a02bc719f3991
zed <158178152@qq.com>
2020-08-20 15:01:27 +0800

    测试动画

--- src/components/animate/animate.vue
+++ src/components/animate/animate.vue
@@ -1,16 +1,18 @@
 <template>
     <section class="animate-container">
-        <button @click="go">clicke me</button>
 
+        <button @click="go">clicke me</button>
 
         <div class="box">
 
+            <div class="box-item box-item-1" :class="{boxItemActiveYi:item_active}">1</div>
+            <div class="box-item box-item-2">2</div>
+            <div class="box-item box-item-3" :class="{boxItemActiveSan:item_active}">3</div>
+            <div class="box-item box-item-4" :class="{boxItemActiveSi:item_active}">4</div>
+            <div class="box-item box-item-5" :class="{boxItemActiveWu:item_active}">5</div>
+            <div class="box-item box-item-6" :class="{boxItemActiveLiu:item_active}">6</div>
 
         </div>
-
-
-        <div :class="{test:animate_name}"></div>
-
 
     </section>
 </template>
@@ -20,13 +22,12 @@
         name: "animate-container",
         data() {
             return {
-                animate_name: false
+                item_active: false
             }
         },
         methods: {
             go() {
-                console.log('1');
-                this.animate_name = true;
+                this.item_active = true;
             }
         }
     }
@@ -34,41 +35,194 @@
 
 <style lang="less" scoped>
 
-
-    .test {
-        width: 100px;
-        height: 100px;
-        animation: test-animation 1s;
-        animation-fill-mode: forwards;
-    }
-
-    @-webkit-keyframes test-animation {
-        0% {
-            background-color: rgba(0, 0, 0, 0)
-        }
-        100% {
-            background-color: rgba(100, 100, 100, 0.5);
-            transform: translateX(200px);
-        }
-    }
-
-    @keyframes test-animation {
-        0% {
-            background-color: rgba(0, 0, 0, 0)
-        }
-        100% {
-            background-color: rgba(100, 100, 100, 0.5);
-            transform: translateX(200px);
-        }
-    }
-
     .box {
+        border: 1px solid red;
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-row-gap: 10px;
+        justify-items: center;
 
         .box-item {
-            margin: 0 auto;
-            border: 1px solid black;
-            width: 50px;
+            width: 70px;
             height: 100px;
+            text-align: center;
+            line-height: 100px;
+            font-size: 30px;
+            color: #fff;
+            transition: all .3s ease-in;
         }
+
+        .box-item-1 {
+            background-color: #2b85e4;
+            transform: translateX(124px);
+            opacity: 0;
+        }
+
+        .boxItemActiveYi {
+            animation: box-item-1-animation 1s;
+            animation-fill-mode: forwards;
+        }
+
+        @-webkit-keyframes box-item-1-animation {
+            0% {
+                transform: translateX(124px);
+                opacity: 0;
+            }
+            100% {
+                transform: translateX(0px);
+                opacity: 1;
+            }
+        }
+
+        @keyframes box-item-1-animation {
+            0% {
+                transform: translateX(124px);
+                opacity: 0;
+            }
+            100% {
+                transform: translateX(0px);
+                opacity: 1;
+            }
+        }
+
+        .box-item-2 {
+            background-color: #515a6e;
+        }
+
+        .box-item-3 {
+            background-color: #19be6b;
+            transform: translateX(-124px);
+            opacity: 0;
+        }
+
+        .boxItemActiveSan {
+            animation: box-item-3-animation 1s;
+            animation-fill-mode: forwards;
+        }
+
+        @-webkit-keyframes box-item-3-animation {
+            0% {
+                transform: translateX(-124px);
+                opacity: 0;
+            }
+            100% {
+                transform: translateX(0px);
+                opacity: 1;
+            }
+        }
+
+        @keyframes box-item-3-animation {
+            0% {
+                transform: translateX(-124px);
+                opacity: 0;
+            }
+            100% {
+                transform: translateX(0px);
+                opacity: 1;
+            }
+        }
+
+        .box-item-4 {
+            background-color: #808695;
+            transform: translate(124px, -110px);
+            opacity: 0;
+        }
+
+        .boxItemActiveSi {
+            animation: box-item-4-animation 1s;
+            animation-fill-mode: forwards;
+            animation-delay: 1s;
+        }
+
+        @-webkit-keyframes box-item-4-animation {
+            0% {
+                transform: translate(124px, -110px);
+                opacity: 0;
+            }
+            100% {
+                transform: translate(0px, 0px);
+                opacity: 1;
+            }
+        }
+
+        @keyframes box-item-4-animation {
+            0% {
+                transform: translate(124px, -110px);
+                opacity: 0;
+            }
+            100% {
+                transform: translate(0px, 0px);
+                opacity: 1;
+            }
+        }
+
+        .box-item-5 {
+            background-color: #ed4014;
+            transform: translateY(-110px);
+            opacity: 0;
+        }
+
+        .boxItemActiveWu {
+            animation: box-item-5-animation 1s;
+            animation-fill-mode: forwards;
+            animation-delay: 1s;
+        }
+
+        @-webkit-keyframes box-item-5-animation {
+            0% {
+                transform: translateY(-110px);
+                opacity: 0;
+            }
+            100% {
+                transform: translateY(0px);
+                opacity: 1;
+            }
+        }
+
+        @keyframes box-item-5-animation {
+            0% {
+                transform: translateY(-110px);
+                opacity: 0;
+            }
+            100% {
+                transform: translateY(0px);
+                opacity: 1;
+            }
+        }
+
+        .box-item-6 {
+            background-color: #5cadff;
+            transform: translate(-124px, -110px);
+            opacity: 0;
+        }
+
+        .boxItemActiveLiu {
+            animation: box-item-6-animation 1s;
+            animation-fill-mode: forwards;
+            animation-delay: 1s;
+        }
+
+        @-webkit-keyframes box-item-6-animation {
+            0% {
+                transform: translate(-124px, -110px);
+                opacity: 0;
+            }
+            100% {
+                transform: translate(0px, 0px);
+                opacity: 1;
+            }
+        }
+
+        @keyframes box-item-6-animation {
+            0% {
+                transform: translate(-124px, -110px);
+                opacity: 0;
+            }
+            100% {
+                transform: translate(0px, 0px);
+                opacity: 1;
+            }
+        }
+
     }
 </style>
