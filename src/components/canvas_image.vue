b559a30aa9640916201234891ce1613cbf41e95b
zed <158178152@qq.com>
2020-09-18 11:43:42 +0800

    canvas合成图

---
+++ src/components/canvas_image.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class="canvas-container">
+        {{message}}
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "canvas_image",
+        props: {
+            message: {
+                type: String
+            }
+        },
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,120 +1,33 @@
 <template>
     <section class="login-container">
 
-        <!--<div class="login-message">-->
-        <!--<div>1</div>-->
-        <!--<div>2</div>-->
-        <!--<div>3</div>-->
-        <!--<div>4</div>-->
-        <!--</div>-->
-
-        <!--<div class="vant-tabs">-->
-        <!--<Tabs v-model="active">-->
-        <!--<Tab v-for="index in 4" :title="'标签 ' + index">内容 1</Tab>-->
-        <!--</Tabs>-->
-        <!--</div>-->
-
-        <!--<div class="button-container">-->
-        <!--<Button round block type="primary" size="small" @click="submit">提交</Button>-->
-        <!--</div>-->
-
-        <tabs></tabs>
-        <animateContainer></animateContainer>
+        <canvas-image ref="canvasImage" :message="message"></canvas-image>
 
     </section>
 </template>
 
 <script>
-    import {Button, Picker, Tab, Tabs} from 'vant';
-    import '../../node_modules/animate.css/animate.css'
-    import tabs from '../components/tabs/tabs'
-    import animateContainer from '../components/animate/animate'
+    import canvasImage from '../components/canvas_image'
 
     export default {
         name: "login",
-        components: {Button, Picker, Tab, Tabs, tabs, animateContainer},
+        components: {canvasImage},
         data() {
             return {
-                active: 0,
-                // ---
-                code_msg: '获取验证码',
-                timer: null
+                message: 'canvas'
             };
         },
         mounted() {
-            let a = new c('你好我是配置', '我是内容');
-            a.consoleInfo()
+            let _canvasImage = this.$refs.canvasImage;
+            console.log(_canvasImage)
         },
-        methods: {
-            submit() {
-                // this.$Alert.info({
-                //     content: '3434'
-                // });
-
-                this.$Occlude.show({prpos_data: '我是一个穿进去的 props'});
-
-                setTimeout(() => {
-                    this.$Occlude.close()
-                }, 3000)
-
-            },
-            getCode() {
-                const TIME_COUNT = 3;
-                if (!this.timer) {
-                    this.code_msg = TIME_COUNT;
-                    this.timer = setInterval(() => {
-                        if (this.code_msg > 0 && this.code_msg <= TIME_COUNT) {
-                            this.code_msg--;
-                        } else {
-                            clearInterval(this.timer);
-                            this.timer = null;
-                            this.code_msg = '获取验证码';
-                        }
-                    }, 1000)
-                }
-            }
-        }
+        methods: {}
     }
 </script>
 
 <style lang="less" scoped>
 
     .login-container {
-        position: relative;
-        height: 100%;
-
-        .vant-tabs {
-
-            .van-tab__pane {
-                border: 1px solid red;
-            }
-        }
-
-        .login-message {
-            border: 1px solid black;
-            padding: 10px;
-            display: grid;
-            grid-template-columns: repeat(2, 1fr);
-            grid-template-rows: repeat(2, 100px);
-            grid-column-gap: 10px;
-            grid-row-gap: 10px;
-            justify-content: end;
-
-            div {
-                border: 1px solid red;
-                text-align: center;
-                line-height: 100px;
-            }
-
-        }
-
-        .button-container {
-            .default-padding-10-20();
-            .default-box-sizing;
-            position: fixed;
-            bottom: 0;
-            left: 0;
-            width: 100%;
-        }
     }
+
 </style>
